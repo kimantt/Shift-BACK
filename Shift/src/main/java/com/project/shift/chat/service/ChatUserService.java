2e224bad9ed62f59094ebb46672272c0211281c8
kimantt <ee2125@naver.com>
2026-04-09 18:56:13 +0900

    S3 관련 코드 주석 처리

--- Shift/src/main/java/com/project/shift/chat/service/ChatUserService.java
+++ Shift/src/main/java/com/project/shift/chat/service/ChatUserService.java
@@ -30,29 +30,29 @@ public class ChatUserService {
 	private final ChatUserDAO chatUserDAO;
 	private final FriendDAO friendDAO;
 	
-	@Value("${cloud.aws.s3.bucket}")
-	private String bucketName;
-	
-	@Value("${cloud.aws.region}")
-	private String region;
-	
-	@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey;
-	
-	@Value("${cloud.aws.credentials.secret-key}")
-	private String secretKey;
-    
-    private S3Client s3;
-
-    @PostConstruct
-    public void initS3() {
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-
-        this.s3 = S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .build();
-    }
+//	@Value("${cloud.aws.s3.bucket}")
+//	private String bucketName;
+//	
+//	@Value("${cloud.aws.region}")
+//	private String region;
+//	
+//	@Value("${cloud.aws.credentials.access-key}")
+//	private String accessKey;
+//	
+//	@Value("${cloud.aws.credentials.secret-key}")
+//	private String secretKey;
+//    
+//    private S3Client s3;
+//
+//    @PostConstruct
+//    public void initS3() {
+//        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+//
+//        this.s3 = S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+//                .build();
+//    }
 	
 	@Transactional(readOnly = true)
 	public ChatUserSearchResultDTO searchUserByPhone(long userId, String phone) {
@@ -90,28 +90,29 @@ public class ChatUserService {
 	// 프로필 이미지 업로드
 	@Transactional
 	public void uploadProfileImage(long userId, MultipartFile file) {
-	    if (file == null || file.isEmpty()) {
-	        throw new IllegalArgumentException("업로드할 파일이 없습니다.");
-	    }
-
-	    String key = "user_profile/" + userId + ".png";
-
-	    try {
-	        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-	                .bucket(bucketName)
-	                .key(key)
-	                .contentType(file.getContentType())
-	                .build();
-
-	        s3.putObject(putObjectRequest,
-	                software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
-	                        file.getInputStream(),
-	                        file.getSize()
-	                ));
-
-	    } catch (Exception e) {
-	        throw new RuntimeException("S3 업로드 실패", e);
-	    }
+//	    if (file == null || file.isEmpty()) {
+//	        throw new IllegalArgumentException("업로드할 파일이 없습니다.");
+//	    }
+//
+//	    String key = "user_profile/" + userId + ".png";
+//
+//	    try {
+//	        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+//	                .bucket(bucketName)
+//	                .key(key)
+//	                .contentType(file.getContentType())
+//	                .build();
+//
+//	        s3.putObject(putObjectRequest,
+//	                software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
+//	                        file.getInputStream(),
+//	                        file.getSize()
+//	                ));
+//
+//	    } catch (Exception e) {
+//	        throw new RuntimeException("S3 업로드 실패", e);
+//	    }
+		throw new RuntimeException("S3 업로드 실패");
 	}
 	
 }
